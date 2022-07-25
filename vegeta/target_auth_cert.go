650bf458ff6f82190128021653830bff1e8cc7c6
David De Leon <56207066+davidadeleon@users.noreply.github.com>
2022-07-25 14:52:19 -0400

    Made recommended improvements to TLS cert requests

--- vegeta/target_auth_cert.go
+++ vegeta/target_auth_cert.go
@@ -12,13 +12,16 @@ import (
 	"encoding/pem"
 	"fmt"
 	"math/big"
+	"net"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/helper/certutil"
 	vegeta "github.com/tsenart/vegeta/v12/lib"
 )
 
@@ -38,35 +41,52 @@ func GenerateCert(caCertTemplate *x509.Certificate, caSigner crypto.Signer) (str
 	// Create the private key
 	signer, keyPEM, err := privateKey()
 	if err != nil {
-		return "", "", err
+		return "", "", fmt.Errorf("error generating private key for server certificate: %v", err)
 	}
 
 	// The serial number for the cert
 	sn, err := serialNumber()
 	if err != nil {
-		return "", "", err
+		return "", "", fmt.Errorf("error generating serial number: %v", err)
+	}
+
+	signerKeyId, err := certutil.GetSubjKeyID(signer)
+	if err != nil {
+		return "", "", fmt.Errorf("error getting subject key id from key: %v", err)
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		return "", "", fmt.Errorf("error getting hostname: %v", err)
+	}
+
+	if hostname == "" {
+		hostname = "localhost"
 	}
 
 	// Create the leaf cert
 	template := x509.Certificate{
-		SerialNumber:          sn,
-		Subject:               pkix.Name{CommonName: "VaultBenchmarkClient"},
-		BasicConstraintsValid: true,
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
-		NotAfter:              time.Now().Add(365 * 24 * time.Hour),
-		NotBefore:             time.Now().Add(-1 * time.Minute),
+		SerialNumber:   sn,
+		Subject:        pkix.Name{CommonName: hostname},
+		KeyUsage:       x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		NotAfter:       time.Now().Add(365 * 24 * time.Hour),
+		NotBefore:      time.Now().Add(-1 * time.Minute),
+		IPAddresses:    []net.IP{net.ParseIP("127.0.0.1")},
+		DNSNames:       []string{"localhost", "localhost4", "localhost6", "localhost.localdomain"},
+		AuthorityKeyId: caCertTemplate.AuthorityKeyId,
+		SubjectKeyId:   signerKeyId,
 	}
 
 	bs, err := x509.CreateCertificate(
 		rand.Reader, &template, caCertTemplate, signer.Public(), caSigner)
 	if err != nil {
-		return "", "", err
+		return "", "", fmt.Errorf("error creating server certificate: %v", err)
 	}
 	var buf bytes.Buffer
 	err = pem.Encode(&buf, &pem.Block{Type: "CERTIFICATE", Bytes: bs})
 	if err != nil {
-		return "", "", err
+		return "", "", fmt.Errorf("error encoding server certificate: %v", err)
 	}
 
 	return buf.String(), keyPEM, nil
@@ -79,44 +99,45 @@ func GenerateCA() (*CaCert, error) {
 	// Create the private key we'll use for this CA cert.
 	signer, _, err := privateKey()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error generating private key for CA: %v", err)
 	}
 
 	// The serial number for the cert
 	sn, err := serialNumber()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error generating serial number: %v", err)
 	}
 
 	signerKeyId, err := keyId(signer.Public())
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error getting subject key id from key: %v", err)
 	}
 
 	// Create the CA cert
 	template := x509.Certificate{
 		SerialNumber:          sn,
-		Subject:               pkix.Name{CommonName: "VaultBenchmarkCA"},
+		Subject:               pkix.Name{CommonName: "Benchmark Vault CA"},
 		BasicConstraintsValid: true,
-		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		KeyUsage:              x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
 		IsCA:                  true,
 		NotAfter:              time.Now().Add(10 * 365 * 24 * time.Hour),
 		NotBefore:             time.Now().Add(-1 * time.Minute),
 		AuthorityKeyId:        signerKeyId,
 		SubjectKeyId:          signerKeyId,
+		IPAddresses:           []net.IP{net.ParseIP("127.0.0.1")},
 	}
 
 	bs, err := x509.CreateCertificate(
 		rand.Reader, &template, &template, signer.Public(), signer)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error creating CA certificate: %v", err)
 	}
 
 	var buf bytes.Buffer
 	err = pem.Encode(&buf, &pem.Block{Type: "CERTIFICATE", Bytes: bs})
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error encoding CA certificate: %v", err)
 	}
 	return &CaCert{
 		PEM:      buf.String(),
