901d32c97eede31807780b4e95a7a61abd23424f
Luis (LT) Carbonell <lt.carbonell@hashicorp.com>
2023-04-24 09:03:28 -0500

    Add mssql benchmark

---
+++ benchmarktests/target_secret_mssql.go
@@ -0,0 +1,233 @@
+package benchmarktests
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/vault/api"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
+
+var flagMSSQLUserConfigJSON string
+
+// Constants for test
+const (
+	MSSQLSecretTestType   = "mssql_secret"
+	MSSQLSecretTestMethod = "GET"
+)
+
+func init() {
+	// "Register" this test to the main test registry
+	TestList[MSSQLSecretTestType] = func() BenchmarkBuilder { return &MSSQLSecret{} }
+}
+
+// Postgres Secret Test Struct
+type MSSQLSecret struct {
+	pathPrefix string
+	roleName   string
+	header     http.Header
+	config     *MSSQLTestConfig
+	logger     hclog.Logger
+}
+
+// Main Config Struct
+type MSSQLTestConfig struct {
+	Config *MSSQLSecretTestConfig `hcl:"config,block"`
+}
+
+// Intermediary struct to assist with HCL decoding
+type MSSQLSecretTestConfig struct {
+	MSSQLDBConfig   *MSSQLDBConfig   `hcl:"db_config,block"`
+	MSSQLRoleConfig *MSSQLRoleConfig `hcl:"role_config,block"`
+}
+
+// MSSQL DB Config
+type MSSQLDBConfig struct {
+	Name                   string   `hcl:"name,optional"`
+	PluginName             string   `hcl:"plugin_name,optional"`
+	PluginVersion          string   `hcl:"plugin_version,optional"`
+	VerifyConnection       *bool    `hcl:"verify_connection,optional"`
+	AllowedRoles           []string `hcl:"allowed_roles,optional"`
+	RootRotationStatements []string `hcl:"root_rotation_statements,optional"`
+	PasswordPolicy         string   `hcl:"password_policy,optional"`
+	ConnectionURL          string   `hcl:"connection_url"`
+	Username               string   `hcl:"username,optional"`
+	Password               string   `hcl:"password,optional"`
+	DisableEscaping        bool     `hcl:"disable_escaping,optional"`
+	MaxOpenConnections     int      `hcl:"max_open_connections,optional"`
+	MaxIdleConnections     int      `hcl:"max_idle_connections,optional"`
+	MaxConnectionLifetime  string   `hcl:"max_connection_lifetime,optional"`
+	UsernameTemplate       string   `hcl:"username_template,optional"`
+	ContainedDB            bool     `hcl:"contained_db,optional"`
+}
+
+// MSSQL Role Config
+type MSSQLRoleConfig struct {
+	Name                 string `hcl:"name,optional"`
+	DBName               string `hcl:"db_name,optional"`
+	DefaultTTL           string `hcl:"default_ttl,optional"`
+	MaxTTL               string `hcl:"max_ttl,optional"`
+	CreationStatements   string `hcl:"creation_statements"`
+	RevocationStatements string `hcl:"revocation_statements,optional"`
+}
+
+// ParseConfig parses the passed in hcl.Body into Configuration structs for use during
+// test configuration in Vault. Any default configuration definitions for required
+// parameters will be set here.
+func (s *MSSQLSecret) ParseConfig(body hcl.Body) error {
+	// provide defaults
+	s.config = &MSSQLTestConfig{
+		Config: &MSSQLSecretTestConfig{
+			MSSQLDBConfig: &MSSQLDBConfig{
+				Name:         "benchmark-mssql",
+				AllowedRoles: []string{"benchmark-role"},
+				PluginName:   "mssql-database-plugin",
+			},
+			MSSQLRoleConfig: &MSSQLRoleConfig{
+				Name:   "benchmark-role",
+				DBName: "benchmark-mssql",
+			},
+		},
+	}
+
+	diags := gohcl.DecodeBody(body, nil, s.config)
+	if diags.HasErrors() {
+		return fmt.Errorf("error decoding to struct: %v", diags)
+	}
+
+	// Handle passed in JSON config
+	if flagMSSQLUserConfigJSON != "" {
+		err := s.config.Config.MSSQLDBConfig.FromJSON(flagMSSQLUserConfigJSON)
+		if err != nil {
+			return fmt.Errorf("error loading test mssql user config from JSON: %v", err)
+		}
+	}
+	return nil
+}
+
+func (s *MSSQLSecret) Target(client *api.Client) vegeta.Target {
+	return vegeta.Target{
+		Method: MSSQLSecretTestMethod,
+		URL:    client.Address() + s.pathPrefix + "/creds/" + s.roleName,
+		Header: s.header,
+	}
+}
+
+func (s *MSSQLSecret) Cleanup(client *api.Client) error {
+	s.logger.Trace(cleanupLogMessage(s.pathPrefix))
+	_, err := client.Logical().Delete(strings.Replace(s.pathPrefix, "/v1/", "/sys/mounts/", 1))
+	if err != nil {
+		return fmt.Errorf("error cleaning up mount: %v", err)
+	}
+	return nil
+}
+
+func (s *MSSQLSecret) GetTargetInfo() TargetInfo {
+	return TargetInfo{
+		method:     MSSQLSecretTestMethod,
+		pathPrefix: s.pathPrefix,
+	}
+}
+
+func (s *MSSQLSecret) Setup(client *api.Client, randomMountName bool, mountName string) (BenchmarkBuilder, error) {
+	var err error
+	secretPath := mountName
+	config := s.config.Config
+	s.logger = targetLogger.Named(MSSQLSecretTestType)
+
+	if randomMountName {
+		secretPath, err = uuid.GenerateUUID()
+		if err != nil {
+			log.Fatalf("can't create UUID")
+		}
+	}
+
+	// Create Database Secret Mount
+	s.logger.Trace(mountLogMessage("secrets", "database", secretPath))
+	err = client.Sys().Mount(secretPath, &api.MountInput{
+		Type: "database",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error mounting db secrets engine: %v", err)
+	}
+
+	setupLogger := s.logger.Named(secretPath)
+
+	// Decode DB Config struct into mapstructure to pass with request
+	setupLogger.Trace(parsingConfigLogMessage("db"))
+	dbData, err := structToMap(config.MSSQLDBConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing db config from struct: %v", err)
+	}
+
+	// Set up db
+	setupLogger.Trace(writingLogMessage("mssql db config"), "name", config.MSSQLDBConfig.Name)
+	dbPath := filepath.Join(secretPath, "config", config.MSSQLDBConfig.Name)
+	_, err = client.Logical().Write(dbPath, dbData)
+	if err != nil {
+		return nil, fmt.Errorf("error writing mssql db config: %v", err)
+	}
+
+	// Decode Role Config struct into mapstructure to pass with request
+	setupLogger.Trace(parsingConfigLogMessage("role"))
+	roleData, err := structToMap(config.MSSQLRoleConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing role config from struct: %v", err)
+	}
+
+	// Create Role
+	setupLogger.Trace(writingLogMessage("mssql role"), "name", config.MSSQLRoleConfig.Name)
+	rolePath := filepath.Join(secretPath, "roles", config.MSSQLRoleConfig.Name)
+	_, err = client.Logical().Write(rolePath, roleData)
+	if err != nil {
+		return nil, fmt.Errorf("error writing mssql role %q: %v", config.MSSQLRoleConfig.Name, err)
+	}
+
+	return &MSSQLSecret{
+		pathPrefix: "/v1/" + secretPath,
+		header:     generateHeader(client),
+		roleName:   config.MSSQLRoleConfig.Name,
+	}, nil
+
+}
+
+func (l *MSSQLSecret) Flags(fs *flag.FlagSet) {
+	fs.StringVar(&flagMSSQLUserConfigJSON, "mssql_test_user_json", "", "When provided, the location of user credentials to test mssql secrets engine.")
+}
+
+func (c *MSSQLDBConfig) FromJSON(path string) error {
+	if path == "" {
+		return fmt.Errorf("no mssql user config passed but is required")
+	}
+
+	// Load JSON config
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(c); err != nil {
+		return err
+	}
+
+	// Check for required fields
+	switch {
+	case c.Username == "":
+		return fmt.Errorf("no username passed but is required")
+	case c.Password == "":
+		return fmt.Errorf("no password passed but is required")
+	default:
+		return nil
+	}
+}
---
+++ examples/secret-mssql.md
@@ -0,0 +1,90 @@
+# MSSQL Secret Configuration Options
+
+This benchmark will test the dynamic generation of MSSQL credentials.
+
+## Test Parameters
+
+### DB Config
+
+- `name` _(string: "benchmark-mssql")_: Name for this database connection. This is specified as part of the URL.
+- `verify_connection` _(optional)_: Specifies if the connection is verified during initial configuration. Defaults to true.
+- `allowed_roles` _(list: ["benchmark_role"])_: List of the roles allowed to use this connection.
+- `root_rotation_statements` _(optional)_: Specifies the database statements to be executed to rotate the root user's credentials.
+- `password_policy` _(optional)_: The name of the password policy to use when generating passwords for this database. If not specified, this will use a default policy defined as: 20 characters with at least 1 uppercase, 1 lowercase, 1 number, and 1 dash character.
+- `connection_url` _(string: <required>)_: Specifies the MSSQL DSN. This field can be templated and supports passing the username and password parameters in the following format {{field_name}}. Certificate authentication can be used by setting ?sslinline=true and giving the SSL credentials in the sslrootcert, sslcert and sslkey credentials. A templated connection URL is required when using root credential rotation. This field supports both format string types, URI and keyword/value. Both formats support multiple host connection strings.
+- `max_open_connections` _(int: 4)_: Specifies the maximum number of open connections to the database.
+- `max_idle_connections` _(int: 0)_: Specifies the maximum number of idle connections to the database.
+- `max_connection_lifetime` _(string: "0s")_: Specifies the maximum amount of time a connection may be reused.
+- `username` _(string: "")_: The root credential username used in the connection URL.
+- `password` _(string: "")_: The root credential password used in the connection URL.
+
+### Role Config
+
+- `name` _(string: "benchmark-role")_: Specifies the name of the role to create. This is specified as part of the URL.
+- `db_name` _(string: "benchmark-mssql")_: The name of the database connection to use for this role.
+- `creation_statements` _(string: <required>)_: Specifies the database statements executed to create and configure a user. See the plugin's API page for more information on support and formatting for this parameter.
+
+## Example Configuration
+
+### Only HCL
+
+```hcl
+test "mssql_secret" "mssql_test_1" {
+    weight = 100
+    config {
+        db_config {
+            connection_url = "sqlserver://{{username}}:{{password}}@localhost:1433"
+            username = "username"
+            password = "P@$$word123"
+        }
+
+        role_config {
+            creation_statements = "CREATE LOGIN [{{name}}] WITH PASSWORD = '{{password}}'; CREATE USER [{{name}}] FOR LOGIN [{{name}}]; GRANT SELECT ON SCHEMA::dbo TO [{{name}}];"
+        }
+    }
+}
+```
+
+```bash
+$ vault-benchmark run -config=example-configs/config.hcl
+Setting up targets...
+Starting benchmarks. Will run for 10s...
+Benchmark complete!
+Target: http://127.0.0.1:8200
+op              count  rate        throughput  mean         95th%        99th%        successRatio
+postgres_test_1  249    248.880537  239.605824  41.018154ms  52.821772ms  58.667201ms  100.00%
+```
+
+### HCL and JSON
+
+```hcl
+test "postgresql_secret" "postgres_test_1" {
+    weight = 100
+    config {
+        db_config {
+            connection_url = "postgresql://{{username}}:{{password}}@localhost:5432/mssql"
+        }
+
+        role_config {
+            creation_statements = "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
+        }
+    }
+}
+```
+
+```json
+{
+    "username": "username",
+    "password": "password"
+}
+```
+
+```bash
+$ vault-benchmark run -config=example-configs/config.hcl -postgres_test_user_json=user.json
+Setting up targets...
+Starting benchmarks. Will run for 10s...
+Benchmark complete!
+Target: http://127.0.0.1:8200
+op              count  rate        throughput  mean         95th%        99th%        successRatio
+postgres_test_1  260    259.460333  245.044207  40.086479ms  59.975645ms  68.40487ms  100.00%
+```
