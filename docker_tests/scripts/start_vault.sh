06be310a2c6831a59054f34c186f674736eb5730
Ellie Sterner <ellie.sterner@hashicorp.com>
2023-05-12 15:25:12 -0400

    adding tests

--- docker_tests/scripts/start_vault.sh
+++ docker_tests/scripts/start_vault.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# install jq
-# echo "Installing jq..."
-# apk add wget
-
-# wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && \
-#     mv jq-linux64 /usr/local/bin/jq && \
-#     chmod +x /usr/local/bin/jq
-
-# unsealing vault
 echo "Unsealing vault..."
 export VAULT_ADDR="http://vault:8200"
 primaryInitRaw=$(vault operator init -format=json -n 1 -t 1)
