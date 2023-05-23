d7150f11657c54838dfb82e452eb89e9a6b9a48c
Ellie Sterner <ellie.sterner@hashicorp.com>
2023-05-23 09:04:25 -0500

    working with simplified startNewService

--- test-fixtures/target_auth_approle_test.go
+++ test-fixtures/target_auth_approle_test.go
@@ -6,11 +6,12 @@ package testfixtures
 import (
 	"testing"
 
-	"github.com/hashicorp/vault-benchmark/docker/postgresqldocker"
+	"github.com/hashicorp/vault-benchmark/docker/benchmarkdocker"
 	// "github.com/hashicorp/vault/helper/testhelpers/etcd"
 )
 
 func TestEtcd3Backend(t *testing.T) {
-	_, _ = postgresqldocker.PrepareTestContainer(t, "13.4-buster")
-
+	// _, _ = postgresqldocker.PrepareTestContainer(t, "13.4-buster")
+	// _, _ = postgrestoodocker.PrepareTestContainer(t, "13.4-buster")
+	_, _ = benchmarkdocker.PrepareTestContainer(t)
 }
