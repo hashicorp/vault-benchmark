a663f51a61217263021aac3bbce39b4423742c1e
Ellie Sterner <ellie.sterner@hashicorp.com>
2023-05-31 11:14:31 -0500

    check exit code once container exits

--- test-fixtures/target_auth_approle_test.go
+++ test-fixtures/target_auth_approle_test.go
@@ -7,12 +7,20 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/vault-benchmark/helper/dockertest"
 )
 
 func TestApprole_Auth_Docker(t *testing.T) {
+	t.Parallel()
+
 	// Create Network
-	networkName := "vault-benchmark"
+	uuid, err := uuid.GenerateUUID()
+	if err != nil {
+		fmt.Errorf("error generating uuid for network name: %w", err)
+	}
+
+	networkName := fmt.Sprintf("vault-benchmark-%v", uuid)
 
 	networkCleanup := dockertest.CreateNetwork(t, networkName)
 	defer networkCleanup()
@@ -23,6 +31,36 @@ func TestApprole_Auth_Docker(t *testing.T) {
 
 	// Run Vault-Benchmark Container
 	vaultAddr := fmt.Sprintf("http:/%s:8200", containerName)
-	benchmarkCleanup := dockertest.CreateVaultBenchmarkContainer(t, networkName, vaultAddr, "root")
-	defer benchmarkCleanup()
+	exitCode := dockertest.CreateVaultBenchmarkContainer(t, networkName, vaultAddr, "root")
+
+	if exitCode != 0 {
+		t.Fatalf("Unexpected error code: %v", exitCode)
+	}
+}
+
+func TestApprole_Auth_Failed_Docker(t *testing.T) {
+	t.Parallel()
+
+	// Create Network
+	uuid, err := uuid.GenerateUUID()
+	if err != nil {
+		fmt.Errorf("error generating uuid for network name: %w", err)
+	}
+
+	networkName := fmt.Sprintf("vault-benchmark-%v", uuid)
+
+	networkCleanup := dockertest.CreateNetwork(t, networkName)
+	defer networkCleanup()
+
+	// Create Vault Container
+	vaultCleanup, containerName := dockertest.CreateVaultContainer(t, networkName)
+	defer vaultCleanup()
+
+	// Run Vault-Benchmark Container
+	vaultAddr := fmt.Sprintf("http:/%s:8200", containerName)
+	exitCode := dockertest.CreateVaultBenchmarkContainer(t, networkName, vaultAddr, "invalid_token")
+
+	if exitCode == 0 {
+		t.Fatalf("Unexpected error code: %v", exitCode)
+	}
 }
