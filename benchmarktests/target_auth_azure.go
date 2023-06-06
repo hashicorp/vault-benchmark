9d42524275498fc46764d76673affec1cde54aaa
Luis (LT) Carbonell <lt.carbonell@hashicorp.com>
2023-06-06 13:06:29 -0500

    Add azure tests

---
+++ benchmarktests/target_auth_azure.go
@@ -0,0 +1,172 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package benchmarktests
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/vault/api"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
+
+// Constants for test
+const (
+	AzureAuthTestType               = "azure_auth"
+	AzureAuthTestMethod             = "POST"
+	AzureAuthTestUserClientIDEnvVar = VaultBenchmarkEnvVarPrefix + "AZURE_TEST_CLIENT_ID"
+	AzureAuthTestUserPasswordEnvVar = VaultBenchmarkEnvVarPrefix + "AZURE_TEST_SECRET"
+)
+
+func init() {
+	// "Register" this test to the main test registry
+	TestList[AzureAuthTestType] = func() BenchmarkBuilder { return &AzureAuth{} }
+}
+
+type AzureAuth struct {
+	pathPrefix string
+	role       string
+	jwt        string
+	header     http.Header
+	config     *AzureAuthTestConfig
+	logger     hclog.Logger
+}
+
+type AzureAuthTestConfig struct {
+	AzureAuthConfig     *AzureAuthConfig     `hcl:"auth,block"`
+	AzureTestUserConfig *AzureTestUserConfig `hcl:"test_user,block"`
+}
+
+type AzureAuthConfig struct {
+	TenantId     string `hcl:"tenant_id"`
+	Resource     string `hcl:"resource"`
+	Environment  string `hcl:"environment,optional"`
+	ClientId     string `hcl:"client_id,optional"`
+	ClientSecret string `hcl:"client_secret,optional"`
+}
+
+type AzureTestUserConfig struct {
+	Role              string `hcl:"role"`
+	JWT               string `hcl:"jwt"`
+	SubscriptionId    string `hcl:"subscription_id"`
+	ResourceGroupName string `hcl:"resource_group_name"`
+	VMName            string `hcl:"vm_name,optional"`
+	VMSSName          string `hcl:"vmss_name,optional"`
+	ResourceId        string `hcl:"resource_id,optional"`
+}
+
+func (l *AzureAuth) ParseConfig(body hcl.Body) error {
+	testConfig := &struct {
+		Config *AzureAuthTestConfig `hcl:"config,block"`
+	}{
+		Config: &AzureAuthTestConfig{
+			AzureAuthConfig: &AzureAuthConfig{
+				ClientId:     os.Getenv(AzureAuthTestUserClientIDEnvVar),
+				ClientSecret: os.Getenv(AzureAuthTestUserPasswordEnvVar),
+			},
+			AzureTestUserConfig: &AzureTestUserConfig{},
+		},
+	}
+
+	diags := gohcl.DecodeBody(body, nil, testConfig)
+	if diags.HasErrors() {
+		return fmt.Errorf("error decoding to struct: %v", diags)
+	}
+	l.config = testConfig.Config
+
+	// Empty Credentials check
+	if l.config.AzureAuthConfig.ClientId == "" {
+		return fmt.Errorf("no client_id provided for vault to use")
+	}
+
+	if l.config.AzureAuthConfig.ClientSecret == "" {
+		return fmt.Errorf("no client_secret provided for vault to use")
+	}
+
+	return nil
+}
+
+func (l *AzureAuth) Target(client *api.Client) vegeta.Target {
+	return vegeta.Target{
+		Method: "POST",
+		URL:    client.Address() + l.pathPrefix + "/login/",
+		Header: l.header,
+		Body:   []byte(fmt.Sprintf(`{"role": "%s", "jwt": "%s"}`, l.role, l.jwt)),
+	}
+}
+
+func (l *AzureAuth) Cleanup(client *api.Client) error {
+	l.logger.Trace(cleanupLogMessage(l.pathPrefix))
+	_, err := client.Logical().Delete(strings.Replace(l.pathPrefix, "/v1/", "/sys/", 1))
+	if err != nil {
+		return fmt.Errorf("error cleaning up mount: %v", err)
+	}
+	return nil
+}
+
+func (l *AzureAuth) GetTargetInfo() TargetInfo {
+	return TargetInfo{
+		method:     AzureAuthTestMethod,
+		pathPrefix: l.pathPrefix,
+	}
+}
+
+func (l *AzureAuth) Setup(client *api.Client, randomMountName bool, mountName string) (BenchmarkBuilder, error) {
+	var err error
+	authPath := mountName
+	config := l.config
+	l.logger = targetLogger.Named(AzureAuthTestType)
+
+	if randomMountName {
+		authPath, err = uuid.GenerateUUID()
+		if err != nil {
+			log.Fatalf("can't create UUID")
+		}
+	}
+
+	// Create Azure Auth mount
+	l.logger.Trace(mountLogMessage("auth", "azure", authPath))
+	err = client.Sys().EnableAuthWithOptions(authPath, &api.EnableAuthOptions{
+		Type: "azure",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error enabling azure: %v", err)
+	}
+
+	setupLogger := l.logger.Named(authPath)
+
+	// Decode AzureConfig struct into mapstructure to pass with request
+	setupLogger.Trace(parsingConfigLogMessage("azure auth"))
+	azureAuthConfig, err := structToMap(config.AzureAuthConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding azure auth config from struct: %v", err)
+	}
+
+	// Write Azure config
+	setupLogger.Trace(writingLogMessage("azure auth config"))
+	_, err = client.Logical().Write("auth/"+authPath+"/config", azureAuthConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error writing azure auth config: %v", err)
+	}
+
+	return &AzureAuth{
+		header:     generateHeader(client),
+		pathPrefix: "/v1/" + filepath.Join("auth", authPath),
+		role:       config.AzureTestUserConfig.Role,
+		jwt:        config.AzureTestUserConfig.JWT,
+		logger:     l.logger,
+	}, nil
+}
+
+// Func Flags accepts a flag set to assign additional flags defined in the function
+func (l *AzureAuth) Flags(fs *flag.FlagSet) {}
---
+++ benchmarktests/target_secret_azure.go
@@ -0,0 +1,168 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package benchmarktests
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/vault/api"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
+
+// Constants for test
+const (
+	AzureSecretTestType   = "azure_secret"
+	AzureSecretTestMethod = "GET"
+)
+
+func init() {
+	// "Register" this test to the main test registry
+	TestList[AzureSecretTestType] = func() BenchmarkBuilder { return &AzureTest{} }
+}
+
+type AzureTest struct {
+	pathPrefix string
+	header     http.Header
+	roleName   string
+	config     *AzureSecretTestConfig
+	logger     hclog.Logger
+}
+
+type AzureSecretTestConfig struct {
+	AzureConfig     *AzureSecretConfig `hcl:"azure,block"`
+	AzureRoleConfig *AzureRoleConfig   `hcl:"role,block"`
+}
+
+type AzureSecretConfig struct {
+	SubscriptionId  string `hcl:"subscription_id"`
+	TenantId        string `hcl:"tenant_id"`
+	ClientId        string `hcl:"client_id,optional"`
+	ClientSecret    string `hcl:"client_secret,optional"`
+	Environment     string `hcl:"environment,optional"`
+	PasswordPolicy  string `hcl:"password_policy,optional"`
+	RootPasswordTTL string `hcl:"root_password_ttl,optional"`
+}
+
+type AzureRoleConfig struct {
+	Name                string `hcl:"name,optional"`
+	AzureRoles          string `hcl:"azure_roles,optional"`
+	AzureGroups         string `hcl:"azure_groups,optional"`
+	ApplicationObjectId string `hcl:"application_object_id,optional"`
+	PersistApp          bool   `hcl:"persist_app,optional"`
+	TTL                 string `hcl:"ttl,optional"`
+	MaxTTL              string `hcl:"max_ttl,optional"`
+	PermanentlyDelete   bool   `hcl:"permanently_delete,optional"`
+}
+
+func (c *AzureTest) ParseConfig(body hcl.Body) error {
+	testConfig := &struct {
+		Config *AzureSecretTestConfig `hcl:"config,block"`
+	}{
+		Config: &AzureSecretTestConfig{
+			AzureConfig:     &AzureSecretConfig{},
+			AzureRoleConfig: &AzureRoleConfig{Name: "benchmark-role"},
+		},
+	}
+
+	diags := gohcl.DecodeBody(body, nil, testConfig)
+	if diags.HasErrors() {
+		return fmt.Errorf("error decoding to struct: %v", diags)
+	}
+	c.config = testConfig.Config
+
+	return nil
+}
+
+func (c *AzureTest) Target(client *api.Client) vegeta.Target {
+	return vegeta.Target{
+		Method: "GET",
+		URL:    client.Address() + c.pathPrefix + "/creds/" + c.roleName,
+		Header: c.header,
+	}
+}
+
+func (c *AzureTest) Cleanup(client *api.Client) error {
+	c.logger.Trace(cleanupLogMessage(c.pathPrefix))
+	_, err := client.Logical().Delete(strings.Replace(c.pathPrefix, "/v1/", "/sys/mounts/", 1))
+	if err != nil {
+		return fmt.Errorf("error cleaning up mount: %v", err)
+	}
+	return nil
+}
+
+func (c *AzureTest) GetTargetInfo() TargetInfo {
+	return TargetInfo{
+		method:     AzureSecretTestMethod,
+		pathPrefix: c.pathPrefix,
+	}
+}
+
+func (c *AzureTest) Setup(client *api.Client, randomMountName bool, mountName string) (BenchmarkBuilder, error) {
+	var err error
+	secretPath := mountName
+	config := c.config
+	c.logger = targetLogger.Named(AzureSecretTestType)
+
+	if randomMountName {
+		secretPath, err = uuid.GenerateUUID()
+		if err != nil {
+			log.Fatalf("can't create UUID")
+		}
+	}
+
+	c.logger.Trace(mountLogMessage("secrets", "azure", secretPath))
+	err = client.Sys().Mount(secretPath, &api.MountInput{
+		Type: "azure",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error mounting azure: %v", err)
+	}
+
+	setupLogger := c.logger.Named(secretPath)
+
+	// Decode Azure Config
+	setupLogger.Trace(parsingConfigLogMessage("azure"))
+	azureConfigData, err := structToMap(config.AzureConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing azure config from struct: %v", err)
+	}
+
+	// Write Azure config
+	setupLogger.Trace(writingLogMessage("azure config"))
+	_, err = client.Logical().Write(secretPath+"/config/access", azureConfigData)
+	if err != nil {
+		return nil, fmt.Errorf("error writing azure config: %v", err)
+	}
+
+	// Decode Role Config
+	setupLogger.Trace(parsingConfigLogMessage("role"))
+	azureRoleConfigData, err := structToMap(config.AzureRoleConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing role config from struct: %v", err)
+	}
+
+	// Create Role
+	setupLogger.Trace(writingLogMessage("azure role"), "name", config.AzureRoleConfig.Name)
+	_, err = client.Logical().Write(secretPath+"/roles/"+config.AzureRoleConfig.Name, azureRoleConfigData)
+	if err != nil {
+		return nil, fmt.Errorf("error writing azure role: %v", err)
+	}
+
+	return &AzureTest{
+		pathPrefix: "/v1/" + secretPath,
+		header:     generateHeader(client),
+		roleName:   config.AzureRoleConfig.Name,
+		logger:     c.logger,
+	}, nil
+}
+
+func (c *AzureTest) Flags(fs *flag.FlagSet) {}
