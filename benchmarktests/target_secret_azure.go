994cce403c5e7c481fb599a33328961ad0b8f79f
Ellie <ellie.sterner@hashicorp.com>
2023-07-27 09:54:09 -0500

    Add azure secrets engine benchmarktest (#137)
    * rebase
    
    * Update docs/tests/secret-azure.md
    
    Co-authored-by: davidadeleon <56207066+davidadeleon@users.noreply.github.com>
    
    * check for required subscription and tenant ID
    
    ---------
    
    Co-authored-by: davidadeleon <56207066+davidadeleon@users.noreply.github.com>

---
+++ benchmarktests/target_secret_azure.go
@@ -0,0 +1,188 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package benchmarktests
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/vault/api"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
+
+// Constants for test
+const (
+	AzureSecretTestType       = "azure_secret"
+	AzureSecretTestMethod     = "GET"
+	AzureSecretSubscriptionID = VaultBenchmarkEnvVarPrefix + "SUBSCRIPTION_ID"
+	AzureSecretTenantID       = VaultBenchmarkEnvVarPrefix + "TENANT_ID"
+	AzureSecretClientID       = VaultBenchmarkEnvVarPrefix + "CLIENT_ID"
+	AzureSecretClientSecret   = VaultBenchmarkEnvVarPrefix + "CLIENT_SECRET"
+	AzureSecretEnvironment    = VaultBenchmarkEnvVarPrefix + "ENVIRONMENT"
+)
+
+func init() {
+	// "Register" this test to the main test registry
+	TestList[AzureSecretTestType] = func() BenchmarkBuilder { return &AzureTest{} }
+}
+
+type AzureTest struct {
+	pathPrefix string
+	header     http.Header
+	roleName   string
+	config     *AzureSecretTestConfig
+	logger     hclog.Logger
+}
+
+type AzureSecretTestConfig struct {
+	AzureConfig *AzureSecretConfig `hcl:"azure,block"`
+	AzureRole   *AzureSecretRole   `hcl:"role,block"`
+}
+
+type AzureSecretConfig struct {
+	SubscriptionId  string `hcl:"subscription_id,optional"`
+	TenantId        string `hcl:"tenant_id,optional"`
+	ClientId        string `hcl:"client_id,optional"`
+	ClientSecret    string `hcl:"client_secret,optional"`
+	Environment     string `hcl:"environment,optional"`
+	PasswordPolicy  string `hcl:"password_policy,optional"`
+	RootPasswordTTL string `hcl:"root_password_ttl,optional"`
+}
+
+type AzureSecretRole struct {
+	Name                string `hcl:"name,optional"`
+	AzureRoles          string `hcl:"azure_roles,optional"`
+	AzureGroups         string `hcl:"azure_groups,optional"`
+	ApplicationObjectId string `hcl:"application_object_id,optional"`
+	PersistApp          bool   `hcl:"persist_app,optional"`
+	TTL                 string `hcl:"ttl,optional"`
+	MaxTTL              string `hcl:"max_ttl,optional"`
+	PermanentlyDelete   bool   `hcl:"permanently_delete,optional"`
+}
+
+func (a *AzureTest) ParseConfig(body hcl.Body) error {
+	testConfig := &struct {
+		Config *AzureSecretTestConfig `hcl:"config,block"`
+	}{
+		Config: &AzureSecretTestConfig{
+			AzureConfig: &AzureSecretConfig{
+				SubscriptionId: os.Getenv(AzureSecretSubscriptionID),
+				TenantId:       os.Getenv(AzureSecretTenantID),
+				ClientId:       os.Getenv(AzureSecretClientID),
+				ClientSecret:   os.Getenv(AzureSecretClientSecret),
+				Environment:    os.Getenv(AzureSecretEnvironment),
+			},
+			AzureRole: &AzureSecretRole{Name: "benchmark-role"},
+		},
+	}
+
+	diags := gohcl.DecodeBody(body, nil, testConfig)
+	if diags.HasErrors() {
+		return fmt.Errorf("error decoding to struct: %v", diags)
+	}
+	a.config = testConfig.Config
+
+	if a.config.AzureConfig.SubscriptionId == "" {
+		return fmt.Errorf("subscription ID is required")
+	}
+
+	if a.config.AzureConfig.TenantId == "" {
+		return fmt.Errorf("tenant ID is required")
+	}
+
+	return nil
+}
+
+func (a *AzureTest) Target(client *api.Client) vegeta.Target {
+	return vegeta.Target{
+		Method: "GET",
+		URL:    client.Address() + a.pathPrefix + "/creds/" + a.roleName,
+		Header: a.header,
+	}
+}
+
+func (a *AzureTest) Cleanup(client *api.Client) error {
+	a.logger.Trace(cleanupLogMessage(a.pathPrefix))
+	_, err := client.Logical().Delete(strings.Replace(a.pathPrefix, "/v1/", "/sys/mounts/", 1))
+	if err != nil {
+		return fmt.Errorf("error cleaning up mount: %v", err)
+	}
+	return nil
+}
+
+func (a *AzureTest) GetTargetInfo() TargetInfo {
+	return TargetInfo{
+		method:     AzureSecretTestMethod,
+		pathPrefix: a.pathPrefix,
+	}
+}
+
+func (a *AzureTest) Setup(client *api.Client, randomMountName bool, mountName string) (BenchmarkBuilder, error) {
+	a.logger = targetLogger.Named(AzureSecretTestType)
+
+	secretPath := mountName
+	if randomMountName {
+		var err error
+		secretPath, err = uuid.GenerateUUID()
+		if err != nil {
+			log.Fatalf("can't create UUID")
+		}
+	}
+
+	config := a.config
+	a.logger.Trace(mountLogMessage("secrets", "azure", secretPath))
+	setupLogger := a.logger.Named(secretPath)
+
+	err := client.Sys().Mount(secretPath, &api.MountInput{
+		Type: "azure",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error mounting azure: %v", err)
+	}
+
+	// Decode Azure Config
+	setupLogger.Trace(parsingConfigLogMessage("azure"))
+	azureConfigData, err := structToMap(config.AzureConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing azure config from struct: %v", err)
+	}
+
+	// Write Azure config
+	setupLogger.Trace(writingLogMessage("azure config"))
+	_, err = client.Logical().Write(secretPath+"/config", azureConfigData)
+	if err != nil {
+		return nil, fmt.Errorf("error writing azure config: %v", err)
+	}
+
+	// Decode Role Config
+	setupLogger.Trace(parsingConfigLogMessage("role"))
+	azureRoleConfigData, err := structToMap(config.AzureRole)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing role config from struct: %v", err)
+	}
+
+	// Create Role
+	setupLogger.Trace(writingLogMessage("azure role"), "name", config.AzureRole.Name)
+	_, err = client.Logical().Write(secretPath+"/roles/"+config.AzureRole.Name, azureRoleConfigData)
+	if err != nil {
+		return nil, fmt.Errorf("error writing azure role: %v", err)
+	}
+
+	return &AzureTest{
+		pathPrefix: "/v1/" + secretPath,
+		header:     generateHeader(client),
+		roleName:   config.AzureRole.Name,
+		logger:     a.logger,
+	}, nil
+}
+
+func (a *AzureTest) Flags(fs *flag.FlagSet) {}
---
+++ docs/tests/secret-azure.md
@@ -0,0 +1,73 @@
+# Azure Authentication Benchmark (`azure_auth`)
+This benchmark will test the dynamic generation of Azure credentials.
+
+## Benchmark Configuration Parameters
+
+### Azure Configuration (`azure`)
+- `subscription_id` (`string: <required>`) - The subscription id for the Azure Active Directory.
+  This value can also be provided with the `VAULT_BENCHMARK_SUBSCRIPTION_ID` environment variable. 
+- `tenant_id` (`string: <required>`) - The tenant id for the Azure Active Directory.
+  This value can also be provided with the `VAULT_BENCHMARK_TENANT_ID` environment variable. 
+- `client_id` (`string:""`) - The OAuth2 client id to connect to Azure. This value can also be provided
+  with the `VAULT_BENCHMARK_CLIENT_ID` environment variable. See [authentication](https://developer.hashicorp.com/vault/docs/secrets/azure#authentication) for more details.
+- `client_secret` (`string:""`) - The OAuth2 client secret to connect to Azure. This value can also be
+  provided with the `VAULT_BENCHMARK_CLIENT_SECRET` environment variable. See [authentication](https://developer.hashicorp.com/vault/docs/secrets/azure#authentication) for more details.
+- `environment` (`string:""`) - The Azure environment. This value can also be provided with the `VAULT_BENCHMARK_ENVIRONMENT`
+  environment variable. If not specified, Vault will use Azure Public Cloud.
+- `password_policy` `(string: "")` - Specifies a [password policy](https://developer.hashicorp.com/vault/docs/concepts/password-policies) to
+  use when creating dynamic credentials. Defaults to generating an alphanumeric password if not set.
+- `root_password_ttl` `(string: "182d")` - Specifies how long the root password is valid for in Azure when
+  rotate-root generates a new client secret. Uses [duration format strings](https://developer.hashicorp.com/vault/docs/concepts/duration-format).
+
+### Azure Role (`role`)
+- `role` (`string: "benchmark-role"`) - Name of role.
+- `azure_roles` (`string: ""`) - List of Azure roles to be assigned to the generated service
+  principal. The array must be in JSON format, properly escaped as a string. See [roles docs](https://developer.hashicorp.com/vault/docs/secrets/azure#roles)
+  for details on role definition.
+- `azure_groups` (`string: ""`) - List of Azure groups that the generated service principal will be
+  assigned to. The array must be in JSON format, properly escaped as a string. See [groups docs](https://developer.hashicorp.com/vault/docs/secrets/azure#azure-groups)
+  for more details.
+- `application_object_id` (`string: ""`) - Application Object ID for an existing service principal that will
+  be used instead of creating dynamic service principals. If present, `azure_roles` will be ignored. See
+  [roles docs](https://developer.hashicorp.com/vault/docs/secrets/azure#roles) for details on role definition.
+- `persist_app` (`bool: "false"`) – If set to true, persists the created service principal and application for the lifetime of the role.
+ Useful for when the Service Principal needs to maintain ownership of objects it creates
+- `ttl` (`string: ""`) – Specifies the default TTL for service principals generated using this role.
+  Accepts time suffixed strings ("1h") or an integer number of seconds. Defaults to the system/engine default TTL time.
+- `max_ttl` (`string: ""`) – Specifies the maximum TTL for service principals generated using this role. Accepts time
+  suffixed strings ("1h") or an integer number of seconds. Defaults to the system/engine max TTL time.
+- `permanently_delete` (`bool: false`) - Specifies whether to permanently delete Applications and Service Principals that are dynamically
+  created by Vault. If `application_object_id` is present, `permanently_delete` must be `false`.
+
+## Example HCL
+
+```hcl
+test "azure_secret" "azure_secret1" {
+    weight = 100
+    config {
+        azure {
+            subscription_id = "subscription_id"
+            tenant_id = "tenant_id"
+            client_id = "client_id"
+            client_secret = "client_secret"
+        }
+
+        role {
+            ttl="1h"
+            application_object_id="application_object_id"
+        }
+    }
+}
+```
+
+## Example Usage
+
+```bash
+$ vault-benchmark run -config=config.hcl
+2023-07-17T11:58:45.488-0500 [INFO]  vault-benchmark: setting up targets
+2023-07-17T11:58:46.039-0500 [INFO]  vault-benchmark: starting benchmarks: duration=10s
+2023-07-17T11:58:59.040-0500 [INFO]  vault-benchmark: benchmark complete
+Target: http://127.0.0.1:8200
+op             count  rate      throughput  mean          95th%         99th%         successRatio
+azure_secret1  47     4.602720  3.615952    2.495158143s  2.854567126s  3.008034917s  100.00%
+```
