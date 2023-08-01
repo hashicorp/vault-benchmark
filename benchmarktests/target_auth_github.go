ac5dc7d42dc5a62a4bd46cb5eae66ba3acb09c01
Luis (LT) Carbonell <lt.carbonell@hashicorp.com>
2023-08-01 10:53:15 -0400

    Add GitHub Auth Engine Tests (#141)
    * Add GitHub Auth Engine Tests
    
    * Fix links
    
    * Update docs/tests/auth-github.md
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    
    * Update docs/tests/auth-github.md
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    
    * Update docs/tests/auth-github.md
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    
    * Update docs/tests/auth-github.md
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    
    * Update docs/tests/auth-github.md
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    
    * Update docs/tests/auth-github.md
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    
    * Update docs/tests/auth-github.md
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    
    * Add disable HTTP/2 flag (#140)
    
    * add disable http2 flag
    
    * update comment
    
    * Remove version newline
    
    * Add azure secrets engine benchmarktest (#137)
    
    * rebase
    
    * Update docs/tests/secret-azure.md
    
    Co-authored-by: davidadeleon <56207066+davidadeleon@users.noreply.github.com>
    
    * check for required subscription and tenant ID
    
    ---------
    
    Co-authored-by: davidadeleon <56207066+davidadeleon@users.noreply.github.com>
    
    * Add changelog file
    
    * Remove Version Newlines (#142)
    
    Add additional check to ensure new lines dont get added the version
    
    * make bool
    
    ---------
    
    Co-authored-by: Ellie <ellie.sterner@hashicorp.com>
    Co-authored-by: davidadeleon <56207066+davidadeleon@users.noreply.github.com>

---
+++ benchmarktests/target_auth_github.go
@@ -0,0 +1,165 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package benchmarktests
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
+	"github.com/hashicorp/vault/api"
+	vegeta "github.com/tsenart/vegeta/v12/lib"
+)
+
+// Constants for test
+const (
+	GitHubAuthTestType      = "github_auth"
+	GitHubAuthTestMethod    = "POST"
+	GitHubAuthTestUserToken = VaultBenchmarkEnvVarPrefix + "GITHUB_TOKEN"
+)
+
+func init() {
+	// "Register" this test to the main test registry
+	TestList[GitHubAuthTestType] = func() BenchmarkBuilder { return &GitHubAuth{} }
+}
+
+type GitHubAuth struct {
+	pathPrefix string
+	token      string
+	header     http.Header
+	config     *GitHubAuthTestConfig
+	logger     hclog.Logger
+}
+
+type GitHubAuthTestConfig struct {
+	GitHubAuthConfig     *GitHubAuthConfig     `hcl:"auth,block"`
+	GitHubTestUserConfig *GitHubTestUserConfig `hcl:"test_user,block"`
+}
+
+type GitHubAuthConfig struct {
+	Organization         string `hcl:"organization"`
+	OrganizationID       string `hcl:"organization_id,optional"`
+	BaseURL              string `hcl:"base_url,optional"`
+	TokenTTL             string `hcl:"token_ttl,optional"`
+	TokenMaxTTL          string `hcl:"token_max_ttl,optional"`
+	TokenPolicies        string `hcl:"token_policies,optional"`
+	Policies             string `hcl:"policies,optional"`
+	TokenBoundCIDRs      string `hcl:"token_bound_cidrs,optional"`
+	TokenExplicitMaxTTL  string `hcl:"token_explicit_max_ttl,optional"`
+	TokenNoDefaultPolicy bool   `hcl:"token_no_default_policy,optional"`
+	TokenNumUses         string `hcl:"token_num_uses,optional"`
+	TokenPeriod          string `hcl:"token_period,optional"`
+	TokenType            string `hcl:"token_type,optional"`
+}
+
+type GitHubTestUserConfig struct {
+	Token string `hcl:"token,optional"`
+}
+
+func (g *GitHubAuth) ParseConfig(body hcl.Body) error {
+	testConfig := &struct {
+		Config *GitHubAuthTestConfig `hcl:"config,block"`
+	}{
+		Config: &GitHubAuthTestConfig{
+			GitHubAuthConfig: &GitHubAuthConfig{},
+			GitHubTestUserConfig: &GitHubTestUserConfig{
+				Token: os.Getenv(GitHubAuthTestUserToken),
+			},
+		},
+	}
+
+	diags := gohcl.DecodeBody(body, nil, testConfig)
+	if diags.HasErrors() {
+		return fmt.Errorf("error decoding to struct: %v", diags)
+	}
+	g.config = testConfig.Config
+
+	// Empty Credentials check
+	if g.config.GitHubTestUserConfig.Token == "" {
+		return fmt.Errorf("no github test user token provided but required")
+	}
+
+	return nil
+}
+
+func (g *GitHubAuth) Target(client *api.Client) vegeta.Target {
+	return vegeta.Target{
+		Method: "POST",
+		URL:    client.Address() + g.pathPrefix + "/login",
+		Header: g.header,
+		Body:   []byte(fmt.Sprintf(`{"token": "%s"}`, g.token)),
+	}
+}
+
+func (g *GitHubAuth) Cleanup(client *api.Client) error {
+	g.logger.Trace(cleanupLogMessage(g.pathPrefix))
+	_, err := client.Logical().Delete(strings.Replace(g.pathPrefix, "/v1/", "/sys/", 1))
+	if err != nil {
+		return fmt.Errorf("error cleaning up mount: %v", err)
+	}
+	return nil
+}
+
+func (g *GitHubAuth) GetTargetInfo() TargetInfo {
+	return TargetInfo{
+		method:     GitHubAuthTestMethod,
+		pathPrefix: g.pathPrefix,
+	}
+}
+
+func (g *GitHubAuth) Setup(client *api.Client, randomMountName bool, mountName string) (BenchmarkBuilder, error) {
+	var err error
+	authPath := mountName
+	g.logger = targetLogger.Named(GitHubAuthTestType)
+
+	if randomMountName {
+		authPath, err = uuid.GenerateUUID()
+		if err != nil {
+			log.Fatalf("can't create UUID")
+		}
+	}
+
+	// Create GitHub Auth mount
+	g.logger.Trace(mountLogMessage("auth", "github", authPath))
+	err = client.Sys().EnableAuthWithOptions(authPath, &api.EnableAuthOptions{
+		Type: "github",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error enabling github: %v", err)
+	}
+
+	setupLogger := g.logger.Named(authPath)
+
+	// Decode GitHubConfig struct into mapstructure to pass with request
+	setupLogger.Trace(parsingConfigLogMessage("github auth"))
+	ldapAuthConfig, err := structToMap(g.config.GitHubAuthConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding github auth config from struct: %v", err)
+	}
+
+	// Write GitHub config
+	setupLogger.Trace(writingLogMessage("github auth config"))
+	_, err = client.Logical().Write("auth/"+authPath+"/config", ldapAuthConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error writing github auth config: %v", err)
+	}
+
+	return &GitHubAuth{
+		header:     generateHeader(client),
+		pathPrefix: "/v1/" + filepath.Join("auth", authPath),
+		token:      g.config.GitHubTestUserConfig.Token,
+		logger:     g.logger,
+	}, nil
+}
+
+// Func Flags accepts a flag set to assign additional flags defined in the function
+func (g *GitHubAuth) Flags(fs *flag.FlagSet) {}
---
+++ docs/tests/auth-github.md
@@ -0,0 +1,54 @@
+# GitHub Auth Benchmark `github_auth`
+
+This benchmark will test GitHub Authentication to Vault. The primary required fields are `organization` and `token`.
+
+## Test Parameters
+
+### Auth Configuration `auth`
+
+- `organization` `(string: <required>)` - The organization users must be part of.
+- `organization_id` `(int: 0)` - The ID of the organization users must be part of. Vault will attempt to fetch and set this value if it is not provided.
+- `base_url` `(string: "")` - The API endpoint to use. Useful if you are running GitHub Enterprise or an API-compatible authentication server.
+- `token_ttl` `(string: "")` - The incremental lifetime for generated tokens. This current value of this will be referenced at renewal time.
+- `token_max_ttl` `(string: "")` - The maximum lifetime for generated tokens. This current value of this will be referenced at renewal time.
+- `token_policies` `(comma-delimited string: "")` - List of token policies to encode onto generated tokens. Depending on the auth method, this list may be supplemented by user/group/other values.
+- `policies` `(comma-delimited string: "")` - DEPRECATED: Please use the `token_policies` parameter instead. List of token policies to encode onto generated tokens. Depending on the auth method, this list may be supplemented by user/group/other values.
+- `token_bound_cidrs` `(comma-delimited string: "")` - List of CIDR blocks; if set, specifies blocks of IP addresses which can authenticate successfully, and ties the resulting token to these blocks as well.
+- `token_explicit_max_ttl` `(string: "")` - If set, will encode an [explicit max TTL](https://developer.hashicorp.com/vault/docs/concepts/tokens#token-time-to-live-periodic-tokens-and-explicit-max-ttls) onto the token. This is a hard cap even if `token_ttl` and `token_max_ttl` would otherwise allow a renewal.
+- `token_no_default_policy` `(bool: false)` - If set, the `default` policy will not be set on generated tokens; otherwise it will be added to the policies set in `token_policies`.
+- `token_num_uses` `(integer: 0)` - The maximum number of times a generated token may be used (within its lifetime); 0 means unlimited. If you require the token to have the ability to create child tokens, you will need to set this value to 0.
+- `token_period` `(string: "")` - The maximum allowed [period](https://developer.hashicorp.com/vault/docs/concepts/tokens#token-time-to-live-periodic-tokens-and-explicit-max-ttls) value when a periodic token is requested from this role.
+- `token_type` `(string: "")` - The type of token that should be generated. Can be `service`, `batch`, or `default` to use the mount's tuned default (which unless changed will be `service` tokens). For token store roles, there are two additional possibilities: `default-service` and `default-batch` which specify the type to return unless the client requests a different type at generation time.
+
+### Test User Config `test_user
+
+- `token` `(string: <required>)` - GitHub personal API token.  This can also be provided via the `VAULT_BENCHMARK_GITHUB_TOKEN` environment variable.
+
+## Example HCL
+
+```hcl
+test "github_auth" "github_auth1" {
+    weight = 100
+    config {
+        auth {
+            organization    = "Test-Organization"
+            organization_id = "12345678910"
+        }
+        test_user  {
+            token = $VAULT_BENCHMARK_GITHUB_TOKEN
+        }
+    }
+}
+```
+
+## Example Usage
+
+```bash
+$ vault-benchmark run -config=github.hcl
+2023-07-25T11:49:39.203-0400 [INFO]  vault-benchmark: setting up targets
+2023-07-25T11:49:39.216-0400 [INFO]  vault-benchmark: starting benchmarks: duration=2s
+2023-07-25T11:49:42.359-0400 [INFO]  vault-benchmark: benchmark complete
+Target: http://127.0.0.1:8200
+op         count  rate       throughput  mean          95th%         99th%         successRatio
+gh_auth_1  31     13.071887  9.864511    833.218959ms  994.991385ms  999.354042ms  100.00%
+```
