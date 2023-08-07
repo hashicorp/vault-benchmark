ccfbb659795afd1c7d284cf3a5af8123a761bc16
Ellie <ellie.sterner@hashicorp.com>
2023-08-07 11:45:13 -0500

    fix setup func to match interface (#148)

--- benchmarktests/target_auth_github.go
+++ benchmarktests/target_auth_github.go
@@ -116,12 +116,12 @@ func (g *GitHubAuth) GetTargetInfo() TargetInfo {
 	}
 }
 
-func (g *GitHubAuth) Setup(client *api.Client, randomMountName bool, mountName string) (BenchmarkBuilder, error) {
+func (g *GitHubAuth) Setup(client *api.Client, mountName string, topLevelConfig *TopLevelTargetConfig) (BenchmarkBuilder, error) {
 	var err error
 	authPath := mountName
 	g.logger = targetLogger.Named(GitHubAuthTestType)
 
-	if randomMountName {
+	if topLevelConfig.RandomMounts {
 		authPath, err = uuid.GenerateUUID()
 		if err != nil {
 			log.Fatalf("can't create UUID")
