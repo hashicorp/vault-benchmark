2cb200ecabe534d4279a2d48a6d1fa3952f40df5
Ellie Sterner <ellie.sterner@hashicorp.com>
2023-08-08 08:25:36 -0500

    update setup func

--- benchmarktests/target_auth_azure.go
+++ benchmarktests/target_auth_azure.go
@@ -142,12 +142,12 @@ func (a *AzureAuth) GetTargetInfo() TargetInfo {
 	}
 }
 
-func (a *AzureAuth) Setup(client *api.Client, randomMountName bool, mountName string) (BenchmarkBuilder, error) {
+func (a *AzureAuth) Setup(client *api.Client, mountName string, topLevelConfig *TopLevelTargetConfig) (BenchmarkBuilder, error) {
 	var err error
 	authPath := mountName
 	a.logger = targetLogger.Named(AzureAuthTestType)
 
-	if randomMountName {
+	if topLevelConfig.RandomMounts {
 		authPath, err = uuid.GenerateUUID()
 		if err != nil {
 			log.Fatalf("can't create UUID")
