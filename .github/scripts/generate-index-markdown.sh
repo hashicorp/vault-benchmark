3f2123d97e2441324df102598b2d08bab02d6ceb
Luis (LT) Carbonell <lt.carbonell@hashicorp.com>
2023-08-28 10:08:52 -0400

    using md template

--- .github/scripts/generate-index-markdown.sh
+++ .github/scripts/generate-index-markdown.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Output file name
 output_file="docs/index.md"
+template_file="${DIR?}"/index-template.md
 
 # Function to extract the title from a Markdown file
 extract_title() {
@@ -27,47 +30,14 @@ generate_tests_list() {
             fi
         fi
     done
-    echo "\n### Auth Benchmark Tests$auth_tests_list\n\n### Secret Benchmark Tests$secret_tests_list\n\n### System Tests$system_tests_list"
+    echo "\n### Auth Benchmark Tests\n$auth_tests_list\n\n### Secret Benchmark Tests\n$secret_tests_list\n\n### System Tests\n$system_tests_list"
 }
 
-# Create the Markdown content
-markdown_content="# Vault Benchmark
-\`vault-benchmark\` has two subcommands, \`run\` and \`review\`. The \`run\` command is the main command used to execute a benchmark run using the provided benchmark test configuration. Configuration is provided as an HCL formatted file containing the desired global configuration options for \`vault-benchmark\` itself as well as the test definitions and their respective configuration options.
+# Read the template file
+template_content=$(<"$template_file")
 
-## Example Config
-\`\`\`hcl
-# Global vault-benchmark config options
-vault_addr = \"http://127.0.0.1:8200\"
-vault_token = \"root\"
-vault_namespace=\"root\"
-duration = \"2s\"
-report_mode = \"terse\"
-random_mounts = true
-cleanup = true
-
-# Test definitions and configuration
-test \"approle_auth\" \"approle_auth_test1\" {
-    weight = 100
-    config {
-        role {
-            role_name = \"benchmark-role\"
-            token_ttl=\"2m\"
-        }
-    }
-}
-\`\`\`
-
-## Subcommands
-- [Run](commands/run.md)
-- [Review](commands/review.md)
-
-## Benchmark Tests
-Below is a list of all currently available benchmark tests
-$(generate_tests_list)
-
-## Global Configuration Options
-- [Global Configuration Options](global-configs.md)
-"
+# Generate the Markdown content
+markdown_content="${template_content//\{\{test_lists_placeholder\}\}/$(generate_tests_list)}"
 
 # Write the Markdown content to the output file
 echo -e "$markdown_content" > "$output_file"
